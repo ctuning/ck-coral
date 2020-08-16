4350ffd712b87e296ed161d3fcf0f23e1f713f0f
Maria Baranova <mari.baranova18@gmail.com>
2020-08-16 09:22:39 +0100

    Add checking for errors

--- package/lib-edgetpu/install.sh
+++ package/lib-edgetpu/install.sh
@@ -8,8 +8,15 @@
 #   
 # Installation script for the EdgeTPU library.
 #
-
+function exit_if_error() {
+  message=${1:-"unknown"}
+  if [ "${?}" != "0" ]; then
+    echo "Error: ${message}!"
+    exit 1
+  fi
+}
 wget --no-cookies ${PACKAGE_URL}${PACKAGE_NAME}
+exit_if_error "wget failed"
 dpkg -x $PACKAGE_NAME .
-
+exit_if_error "dpkg failed"
 return 0
